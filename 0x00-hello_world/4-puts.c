008d5f993c3815eeb317f051f56004f58cf06c13
karim bahri <karim-bahri-18@outlook.fr>
2020-02-05 12:42:26 +0000

    printf commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*MAIN FUNCTION*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*MAIN FUNCTION*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
