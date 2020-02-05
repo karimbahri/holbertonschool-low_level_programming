bf4221812626529b059e8af5ae1715391768b82f
karim bahri <karim-bahri-18@outlook.fr>
2020-02-05 13:25:10 +0000

    sizeof commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void)
-        {
-
-		return 0;
-        }
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+return (0);
+}
