a7add1223fb65855981272d92d89ab9b64f769ac
karim bahri <karim-bahri-18@outlook.fr>
2020-02-06 18:23:43 +0000

    comb3 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - print comb3
+*Return: 0
+*/
+int main(void)
+{
+	int number1 = 0, number2 = 0;
+
+	for (number1 = 48 ; number1 < 58 ; number1++)
+		for (number2 = 48 ; number2 < 58 ; number2++)
+		{
+			if (number1 != number2 && number1 < number2)
+			{
+				putchar(number1);
+				putchar(number2);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
