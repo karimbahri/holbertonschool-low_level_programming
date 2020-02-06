f02004ac9b254dfc6a140078c9eefaf6c77c5e12
karim bahri <karim-bahri-18@outlook.fr>
2020-02-06 12:58:55 +0000

    numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main : display digits
+*Return: 0
+*/
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0 ; number < 10 ; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
