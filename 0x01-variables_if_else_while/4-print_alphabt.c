c410b31ceb20d228516864d8257396345efbc673
karim bahri <karim-bahri-18@outlook.fr>
2020-02-06 12:38:17 +0000

    without e q commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - display exception
-*Result: 0
+*Return: 0
 */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	{
 		if (character == 'q' || character == 'e')
-			continue;
+			putchar('');
 		putchar(character);
 	}
 	putchar('\n');
