7a3e061aa82249c9f166daaeb51c4d9cdf7686ee
karim bahri <karim-bahri-18@outlook.fr>
2020-02-11 13:12:51 +0000

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,60 +1,57 @@
 #include "holberton.h"
 
 /**
-* times_table - print 9 time table
+*times_table - print 9 time table
 */
 void times_table(void)
 {
 	int i = 0, j = 0, mult = 0;
-	char u, d;
+	char u = 0, d = 0;
 
 	for (i = 0 ; i <= 9 ; i++)
+	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
 			mult = i * j;
+			u = mult % 10;
+			d = mult / 10;
+
 			if (mult > 9)
 			{
-				u = (mult % 10) + '0';
-				d = (mult / 10) + '0';
-				_putchar(d);
-				_putchar(u);
-				if (j < 9)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
-				else
-				{
-					_putchar('$');
-					_putchar('\n');
-				}
+				_putchar(d + '0');
+				_putchar(u + '0');
+				checkNumber(j, 0);
 			}
 			else
 			{
-				if (j < 9)
-				{
-				_putchar(mult + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				}
-				else
-				{
-					_putchar('$');
-					_putchar('\n');
-				}
-			}
-
-			if (j < 9)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			}
-			else
-			{
-				_putchar('$');
-				_putchar('\n');
+				_putchar(u + '0');
+				checkNumber(j, 1);
 			}
 		}
+	}
+}
+
+/**
+*checkNumber - check num is equal to 9
+*@num: the number to check
+*@state: check that state is true
+*/
+void checkNumber(int num, int state)
+{
+	if (num < 9 && state)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else if (num < 9 && !state)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('$');
+		_putchar('\n');
+	}
 }
