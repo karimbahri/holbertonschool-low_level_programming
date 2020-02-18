36600f90e2e9d0700dcb1136140848b4576c4d34
karim bahri <karim-bahri-18@outlook.fr>
2020-02-18 10:58:48 +0000

    convert to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert string to integer
+ * @s: string to convert
+ * Return: the integer
+ */
+int _atoi(char *s)
+{
+	int integer = 0, i = 0, sign = 1;
+
+	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		i++;
+	}
+
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			integer = (integer * 10) + (s[i] - '0');
+
+		i++;
+	}
+
+	integer *= sign;
+
+	return (integer);
+}
