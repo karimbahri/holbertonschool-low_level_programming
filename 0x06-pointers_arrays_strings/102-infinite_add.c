ed47d112d92257c22220cf193fe1488230707437
karim bahri <karim-bahri-18@outlook.fr>
2020-02-20 20:59:05 +0000

    infinite add

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -4,7 +4,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int sum = 0, lengthN1 = _strlen(n1), lengthN2 = _strlen(n2) , i = 1;
 
-	while (i == size_r)
+	while (i != size_r)
 	{
 		sum = (n1[lengthN1 - i] - '0') + (n2[lengthN2 - i] - '0');
 		if (sum > 9)
@@ -13,7 +13,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			r[size_r - i - 1] = (sum / 10) + '0';
 		}
 		else
-			r[size_r - i] = sum;
+			r[size_r - i] = sum + '0';
 
 		i++;
 	}
