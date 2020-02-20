cf691645f39312b5ce0d41968e39986db336f9c3
karim bahri <karim-bahri-18@outlook.fr>
2020-02-20 12:09:07 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,8 +10,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, cmp = 0; 
 
-	while (!(cmp = s1[i] - s2[i]))
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		
+		cmp = s1[i] - s2[i];
+		if (cmp)
+			break;
+
+		i++;
 	}
+
+	return (cmp);
 }
