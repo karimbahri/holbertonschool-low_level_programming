eadb07f074895a368716f141f50234acac927bf4
karim bahri <karim-bahri-18@outlook.fr>
2020-02-20 15:30:44 +0000

    encryption

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,34 +7,19 @@
  */
 char *leet(char *s)
 {
-	int i = 0, j = 0, length = _strlen(s);
-	char key[] = {'4', '3', '0', '7', '1'}, character[] = {'a', 'e', 'o', 't', 'l'};
+	int i = 0, j = 0;
+	char key[] = {'4', '3', '0', '7', '1'};
+	char character[] = {'a', 'e', 'o', 't', 'l'};
 
-	for (i = 0 ; i < length ; i++)
+	while (s[i] != '\0')
 	{
 		for (j = 0 ; j < 5 ; j++)
 		{
 			if (s[i] == character[j] || s[i] == (character[j] - ('a' - 'A')))
 				s[i] = key[j];
 		}
+		i++;
 	}
 
 	return (s);
-}
-
-/**
- * _strlen - return the length of a string
- * @s: the string to check
- * Return: lenght
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	return (length);
 }
