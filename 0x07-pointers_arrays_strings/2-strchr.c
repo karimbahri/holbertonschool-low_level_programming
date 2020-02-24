e2cacfca68487905afcc72e2fa9134097a1a7e82
karim bahri <karim-bahri-18@outlook.fr>
2020-02-24 11:34:48 +0000

    strchr fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,9 +14,12 @@ char *_strchr(char *s, char c)
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			break;
 	}
 
-	return (NULL);
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
 
 }
