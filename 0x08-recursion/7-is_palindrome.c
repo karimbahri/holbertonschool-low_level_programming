dbc72f9c13ed4c9f81af13b4f06db26c8d823b8d
karim bahri <karim-bahri-18@outlook.fr>
2020-02-26 18:55:48 +0000

    palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -25,7 +25,7 @@ int is_palindrome(char *s)
 int check_palindrome(char *s, int pos)
 {
 	if (!*s)
-		return (1);	
+		return (1);
 
 	if (s[pos] == s[_strlen_recursion(s) - pos - 1] &&
 	pos + 1 < _strlen_recursion(s))
