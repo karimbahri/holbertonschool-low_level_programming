5bd628ba719cc240e30dd6b98d78fec06b8e3509
karim bahri <karim-bahri-18@outlook.fr>
2020-03-03 14:32:48 +0000

    2D ARRAY

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - 2D array dynamic allocation
+ * @width: width
+ * @height: height
+ * Return: 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, **grid = NULL;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int*));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < height ; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	return (grid);
+}
