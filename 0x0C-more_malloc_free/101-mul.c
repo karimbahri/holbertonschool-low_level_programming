17ee04b1077eb0ed2c0a96e77a5f5b7243f82bc5
karim bahri <karim-bahri-18@outlook.fr>
2020-03-05 18:30:39 +0000

    mult

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - display the result of argv[1] multiplied with argv[2]
+ * @argc: size of argv
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
