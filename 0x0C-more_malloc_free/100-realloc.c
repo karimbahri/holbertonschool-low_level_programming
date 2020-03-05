dd02e0f232652f0103a912551de630f309a3676e
karim bahri <karim-bahri-18@outlook.fr>
2020-03-05 17:57:25 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "holberton.h"
 
+/**
+ * _realloc - reserve new spaces of memory for an existing array
+ * @ptr: existing array
+ * @old_size: size of ptr
+ * @new_size: size of the new array
+ * Return: dynamic array
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
@@ -29,7 +36,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	return (dynamic_array);
 }
 
-
+/**
+ * set_memory - copy the containing of src into array
+ * @array: destination
+ * @src: source
+ * @new_size: new size
+ * @old_size: old size
+ */
 void set_memory(char *array, char *src, int new_size, int old_size)
 {
 	int i = 0;
