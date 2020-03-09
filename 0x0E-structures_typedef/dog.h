0fad0a496d744a4354dc3a0f06fc69efbcde364f
karim bahri <karim-bahri-18@outlook.fr>
2020-03-09 12:05:54 +0000

    create dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef Django
+#define Django
+/**
+ * dog - variable of type dog
+ * @name: dog's name
+ * @owner: dog's owner name
+ * @age: dog's age
+ * Description: a dog type with name , owner's name & age attributes
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
