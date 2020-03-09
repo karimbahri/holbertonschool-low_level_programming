586b5ba052adc7dd7fa11c3b2427ae9409e27585
karim bahri <karim-bahri-18@outlook.fr>
2020-03-09 12:43:17 +0000

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("name: (nil)\n");
+			printf("Name: (nil)\n");
 
 		printf("Age: %f\n", d->age);
 
