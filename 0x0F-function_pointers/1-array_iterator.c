8861d8fda54208c6b9a0c98fbbb82f0e71d0ea11
karim bahri <karim-bahri-18@outlook.fr>
2020-03-11 11:16:16 +0000

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "function_pointers.h"
-
+/**
+ * array_iterator - iterate array
+ * @array: array to iterate
+ * @size: size of array
+ * @action: fonction 
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
