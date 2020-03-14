404c5dcb2455293c9a73b38957a128ba38c9a268
karim bahri <karim-bahri-18@outlook.fr>
2020-03-14 22:47:40 +0000

    fix print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,77 +4,47 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - print a char
- * @list_of_variables: character to print
- */
-void print_char(va_list list_of_variables)
-{
-	printf("%c", va_arg(list_of_variables, int));
-}
-
-/**
- * print_int - print an integer
- * @list_of_variables: integer to print
- */
-void print_int(va_list list_of_variables)
-{
-	printf("%d", va_arg(list_of_variables, int));
-}
-
-/**
- * print_float - print a float
- * @list_of_variables: float to print
- */
-void print_float(va_list list_of_variables)
-{
-	printf("%f", va_arg(list_of_variables, double));
-}
-
-/**
- * print_str - print a string
- * @list_of_variables: string to print
- */
-void print_str(va_list list_of_variables)
-{
-	if (!list_of_variables)
-	{
-		printf("(nil)");
-		return;
-	}
-		printf("%s", va_arg(list_of_variables, char*));
-}
-
-/**
  * print_all - print any type of variable
  * @format: type desired
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j = 0;
 	va_list list_of_variables;
-	char *sep = "";
-	TYPES t[5] = {{'c', print_char}, {'i', print_int},
-					{'f', print_float}, {'s', print_str},
-					{0, NULL} };
+	int i = 0;
+	bool is_type = TRUE;
+	char *txt;
 
 	va_start(list_of_variables, format);
-	while (format[i])
+	while (format && format[i])
 	{
-		while (t[j].type_format)
+		switch (format[i])
 		{
-			if (format[i] == t[j].type_format)
-			{
-				printf("%s", sep);
-				t[j].print_type(list_of_variables);
-				sep = ", ";
-				break;
-			}
-			j++;
+			case 'i':
+				printf("%d", va_arg(list_of_variables, int));
+					break;
+			case 'c':
+				printf("%c", va_arg(list_of_variables, int));
+					break;
+			case 'f':
+				printf("%f", va_arg(list_of_variables, double));
+					break;
+			case 's':
+				txt = va_arg(list_of_variables, char*);
+				if (!txt)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", txt);
+					break;
+			default:
+				is_type = FALSE;
+					break;
 		}
-		i++, j = 0;
+		if (is_type && format[i + 1])
+			printf(", ");
+		i++;
 	}
 	NEWLINE;
-
 	va_end(list_of_variables);
-
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,26 +3,20 @@
 
 #define NEWLINE printf("\n")
 #include <stdarg.h>
-/*--------------------STRUCTS---------------------*/
 /**
- * struct TYPES - types format and functions to perform the display
- * @type_format: character
- * @print_type: function to print
+ * enum bool - boolean type
+ * @FALSE: false value
+ * @TRUE: true value
  */
-typedef struct TYPES
+typedef enum bool
 {
-	char type_format;
-	void (*print_type)(va_list);
-
-} TYPES;
+	FALSE,
+	TRUE
+} bool;
 
 /*-------------------PROTOTYPES-------------------*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list list_of_variables);
-void print_int(va_list list_of_variables);
-void print_float(va_list list_of_variables);
-void print_str(va_list list_of_variables);
 #endif
