bbed22132d92cf795d4fd59b0ec17454a554637e
karim bahri <karim-bahri-18@outlook.fr>
2020-03-22 15:20:48 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startup - function executed before main function
+ */
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
