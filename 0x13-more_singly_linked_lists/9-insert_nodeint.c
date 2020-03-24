0ddd73e34060b6667dcbeb0265149c5f8f69b310
karim bahri <karim-bahri-18@outlook.fr>
2020-03-24 15:41:46 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node = *head, *ins_node, *next;
 	unsigned int pos = 0;
 
+	if (!head)
+		return (NULL);
+
 	ins_node = malloc(sizeof(listint_t));
 
 	if (!ins_node)
