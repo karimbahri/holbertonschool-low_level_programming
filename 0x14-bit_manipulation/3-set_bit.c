b0443d31e04080f2760f9e3daec025eb85ef5a0e
karim bahri <karim-bahri-18@outlook.fr>
2020-03-26 17:50:39 +0000

    set bit at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * set_bit - set bit at index
+ * @n: integer to change
+ * @index: index
+ * Return: 1 if true -1 otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+
+	*n = *n | 1 << index;
+
+	return (1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,4 +4,5 @@ unsigned int binary_to_uint(const char *b);
 unsigned long int power(unsigned int a, unsigned int b);
 int _strlen(const char *s);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
