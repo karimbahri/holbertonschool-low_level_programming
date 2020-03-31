4b36348d3459174e27b365077d58856f26358cc2
karim bahri <karim-bahri-18@outlook.fr>
2020-03-31 21:49:55 +0000

    fix cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,13 +50,13 @@ int main(int argc, char *argv[])
 	if (destination_file_d == -1)
 		exitWithError("Error: Can't write to NAME_OF_THE_FILE", argv[2], 99);
 
-	do {
-		nb_R = read(source_file_d, buffer, 1024);
+	while ((nb_R = read(source_file_d, buffer, 1024)))
+	{
 		if (nb_R == -1)
 			exitWithError("Error: Can't read from file", argv[1], 98);
 		if (write(destination_file_d, buffer, nb_R) == -1)
 			exitWithError("Error: Can't write to NAME_OF_THE_FILE", argv[2], 99);
-	} while (nb_R);
+	}
 
 	if (close(source_file_d) == -1)
 	{
