988b4c67ed74e133133cefae37d53559bd1d5a94
karim bahri <karim-bahri-18@outlook.fr>
2020-03-31 16:36:43 +0000

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "holberton.h"
+
+/**
+ * append_text_to_file - append text to file
+ * @filename: path of the file
+ * @text_content: content to append
+ * Return: 1 if it works , -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string to check
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
