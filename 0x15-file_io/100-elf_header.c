ef36b6e2b34ff53bf0e6ee42ce72cceeb4987fd0
karim bahri <karim-bahri-18@outlook.fr>
2020-03-31 22:57:32 +0000

    elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/**
+ * main - main function
+ * @argc: size of argc
+ * @argv: array of integer
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+
+	if (argc != 3)
+	{
+		dprintf(2, "usage elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY)
+	if (fd == -1)
+	{
+		dprintf(2, "couldn't open file\n");
+		exit(98);
+	}
+
+	close(fd);
+	return (EXIT_SUCCESS);
+}
