d3fae2fecd34387e591f5e1137760d56b3e35ff6
karim bahri <karim-bahri-18@outlook.fr>
2020-03-31 21:43:31 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,44 +34,40 @@ void exitWithError(const char *log, const char *fileName, int status)
 int main(int argc, char *argv[])
 {
 	char buffer[1024];
-	int source_file_d, destination_file_d;
+	int source_file_d, destination_file_d, nb_R;
 
 	if (argc != 3)
 		exitWithError("Usage: cp file_from file_to", NULL, 97);
 
 	source_file_d = open(argv[1], O_RDONLY);
 
-	if (!source_file_d)
+	if (source_file_d == -1)
 		exitWithError("Error: Can't read from file", argv[1], 98);
 
 	destination_file_d = open(argv[2],
 	O_WRONLY | O_TRUNC | O_CREAT,
 	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	if (!destination_file_d)
+	if (destination_file_d == -1)
 		exitWithError("Error: Can't write to NAME_OF_THE_FILE", argv[2], 99);
 
-	while (*buffer)
-	{
-		if (read(source_file_d, buffer, 1024) == -1)
+	do {
+		nb_R = read(source_file_d, buffer, 1024);
+		if (nb_R == -1)
 			exitWithError("Error: Can't read from file", argv[1], 98);
-		if (write(destination_file_d, buffer, 1024) == -1)
+		if (write(destination_file_d, buffer, nb_R) == -1)
 			exitWithError("Error: Can't write to NAME_OF_THE_FILE", argv[2], 99);
-
-	}
+	} while (nb_R);
 
 	if (close(source_file_d) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", source_file_d);
 		exit(100);
 	}
-
 	if (close(destination_file_d) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", destination_file_d);
 		exit(100);
 	}
-
 	return (EXIT_SUCCESS);
 }
 
