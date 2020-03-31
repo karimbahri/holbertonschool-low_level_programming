1fc649423f5b80f01840ac771bb0fd08082b19ab
karim bahri <karim-bahri-18@outlook.fr>
2020-03-31 22:44:52 +0000

    fix cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,13 +50,12 @@ int main(int argc, char *argv[])
 	if (destination_file_d == -1)
 		exitWithError("Error: Can't write to", argv[2], 99);
 
-	while ((nb_R = read(source_file_d, buffer, 1024)))
-	{
-		if (nb_R == -1)
-			exitWithError("Error: Can't read from file", argv[1], 98);
+	while ((nb_R = read(source_file_d, buffer, 1024)) > 0)
 		if (write(destination_file_d, buffer, nb_R) == -1)
 			exitWithError("Error: Can't write to", argv[2], 99);
-	}
+
+	if (nb_R == -1)
+		exitWithError("Error: Can't read from file", argv[1], 98);
 	if (close(source_file_d) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", source_file_d);
@@ -69,5 +68,3 @@ int main(int argc, char *argv[])
 	}
 	return (EXIT_SUCCESS);
 }
-
-
