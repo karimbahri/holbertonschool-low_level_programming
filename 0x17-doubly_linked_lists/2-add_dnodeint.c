0252450e6146b8d1fea6a44b07c85a3f6e12c0ed
karim bahri <karim-bahri-18@outlook.fr>
2020-05-02 13:35:11 +0000

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,8 +19,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	node->n = n;
-	node->prev = NULL;
+
+	if (!head || !*head)
+	{
+		node->next = NULL;
+		node->prev = NULL;
+
+	}
+
+	else
+	{
 	node->next = *head;
+	node->prev = NULL;
+	(*head)->prev = node;
+	}
+
 	*head = node;
 
 	return (node);
