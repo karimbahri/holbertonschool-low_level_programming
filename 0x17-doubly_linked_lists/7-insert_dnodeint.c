9a97c98716e541ac5593bf979563fa31cadd083f
karim bahri <karim-bahri-18@outlook.fr>
2020-05-03 14:07:34 +0000

    fix insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *node, *pos_list;
 	unsigned int i = 0;
 
-	if (!*h)
-		return (NULL);
-
 	pos_list = *h;
 
 	if (!idx)
