c5ab3c3819f1d7db97d5e74353ff19dde0837438
karim bahri <karim-bahri-18@outlook.fr>
2020-05-03 02:38:31 +0000

    add index using prev functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,35 +18,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	pos_list = *h;
+
+	if (!idx)
+	{
+		node = add_dnodeint(h, n);
+		return (node);
+	}
+
 	node = malloc(sizeof(dlistint_t));
+
 	if (!node)
 		return (NULL);
 
 	node->n = n;
-	if (!idx)
-	{
-		if (*h)
-			(*h)->prev = node;
 
-		node->next = *h;
-		node->prev = NULL;
-		*h = node;
-		return (node);
-	}
-	else
+	while (pos_list)
 	{
-		while (pos_list)
+		if (i == idx)
 		{
-			if (i == idx - 1)
-			{
-				node->next = pos_list->next;
-				pos_list->next = node;
-				node->prev = pos_list;
-				break;
-			}
-			i++;
-			pos_list = pos_list->next;
+			pos_list->prev->next = node;
+			node->prev = pos_list->prev;
+			node->next = pos_list;
+			pos_list->prev = node;
+			return (node);
 		}
+		i++;
+		pos_list = pos_list->next;
+	}
+	if (i == idx)
+	{
+		node = add_dnodeint_end(h, n);
 		return (node);
 	}
 	return (NULL);
