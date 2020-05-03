3c8584f617ef56653a06e115ee573bee9c0ca3a0
karim bahri <karim-bahri-18@outlook.fr>
2020-05-03 17:47:20 +0000

    keygen

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - keygen
+ * @argc: size of argv
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[1]);
+	return (EXIT_SUCCESS);
+}
