065090eb22cc4cee9510d2168e186be9adad12f0
karim bahri <karim-bahri-18@outlook.fr>
2020-05-16 02:39:55 +0000

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,15 +8,7 @@
  */
 int main(void)
 {
-	char passwd[20] = {0};
-	int i = 0;
-
-	srand(time(NULL));
-
-	for (i = 0 ; i < 7 ; i++)
-	{
-		passwd[i] = (rand() % (122 - 48)) + 48;
-	}
+	char passwd[] = "dddddddddddddddddddddddddddH";
 	printf("%s", passwd);
 	return (0);
 }
