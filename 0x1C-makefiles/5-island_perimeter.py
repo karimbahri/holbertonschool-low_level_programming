58a2c6c4dbb4daa0b19d0c81d7311b7f37fcf345
karim bahri <karim-bahri-18@outlook.fr>
2020-07-04 17:51:05 +0000

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""island_perimeter"""
+
+
+def island_perimeter(grid):
+    """island_perimeter function"""
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or j == 0:
+                    per += 1
+                if i == len(grid) - 1 or j == len(grid[i]) - 1:
+                    per += 1
+                if grid[i - 1][j] == 0:
+                    per += 1
+                if grid[i + 1][j] == 0:
+                    per += 1
+                if grid[i][j - 1] == 0:
+                    per += 1
+                if grid[i][j + 1] == 0:
+                    per += 1
+    return per
